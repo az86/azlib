3bf78ae0118c7c6f6d68501a1e6c7508d40dcc03
az <m_sunqi@qq.com>
2018-04-13 13:28:44 +0800

    create

---
+++ src/AZDotNet.Basic/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZDotNet.Basic
+{
+    public class Logger
+    {
+    }
+}
