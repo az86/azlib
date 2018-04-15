c2747e59ee5d80814fb2ec5ab20b1fb120ffa332
az <m_sunqi@qq.com>
2018-04-15 16:26:26 +0800

    modify logger class

--- src/AZDotNet.Basic/Logger.cs
+++ src/AZDotNet.Basic/Logger.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AZDotNet.Basic
 {
-    public class Logger
+    public static class Logger
     {
+        public static void WriteDebug(string content, [CallerMemberName]string name = "Unknown")
+        {
+            Console.WriteLine("{0}: {1}", name, content);
+        }
+
+        public static void WriteWarning(string content, [CallerMemberName]string name = "Unknown")
+        {
+            Console.WriteLine("{0}: {1}", name, content);
+        }
+
+        public static void WriteError(string content, [CallerMemberName]string name = "Unknown")
+        {
+            Console.WriteLine("{0}: {1}", name, content);
+        }
     }
 }
